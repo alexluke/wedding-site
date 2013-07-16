3c0827b0786adeb64910dd214317c8cc2a262653
Alex Luke <alex@alexluke.me>
2013-07-16 09:11:48 -0700

    Fully qualify config import

--- wedding/__init__.py
+++ wedding/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask.ext.mail import Mail
 
 app = Flask(__name__.split('.')[0])
-app.config.from_object('envconf')
+app.config.from_object('wedding.envconf')
 mail = Mail(app)
 
 import views
