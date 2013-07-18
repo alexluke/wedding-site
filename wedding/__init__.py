69cf2e36f671d8de340da53e83c2e565ad6d1b4b
Alex Luke <alexl@bnj.com>
2013-07-18 09:56:42 -0700

    Authenticate mail sent on errors

--- wedding/__init__.py
+++ wedding/__init__.py
@@ -5,14 +5,15 @@ app = Flask(__name__.split('.')[0])
 app.config.from_object('wedding.envconf')
 mail = Mail(app)
 
-if int(app.debug) == 0:
+if not app.debug:
     import logging
     from logging.handlers import SMTPHandler
 
-    mail_handler = SMTPHandler(app.config['MAIL_SERVER'],
+    mail_handler = SMTPHandler((app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
         app.config['MAIL_DEFAULT_SENDER'],
         'alex@alexluke.me',
-        'Exception on wedding site')
+        'Exception on wedding site',
+        credentials=(app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD']))
     mail_handler.setLevel(logging.ERROR)
     app.logger.addHandler(mail_handler)
 
